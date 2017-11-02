529a5419e5c763dc8b1deb251e35c1990eb84ed6
Martin M <Martin M@MEMEEP>
2017-11-02 15:37:44 +0100

    last commit

--- Stack_Michalitsch_Wi16b157/Program.cs
+++ Stack_Michalitsch_Wi16b157/Program.cs
@@ -13,6 +13,7 @@ namespace Stack_Michalitsch_Wi16b157
             Console.WriteLine("Branch 2 Commit 2");
             Console.WriteLine("Branch 2 Commit 3");
             Console.WriteLine("Branch 3 Commit 3");
+            Console.WriteLine("Branch 3 Commit 3");
             Stack<String> MyStack = new Stack<String>();
             MyStack.Push(Console.ReadLine());
             MyStack.Push(Console.ReadLine());
